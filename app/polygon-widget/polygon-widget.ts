427ca6129ca9b028b42df068a43900dcd498c2b4
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-04-27 01:54:11 +0200

    prob el style sw

--- app/polygon-widget/polygon-widget.ts
+++ app/polygon-widget/polygon-widget.ts
@@ -25,6 +25,7 @@ import { validInput } from "./validation";
 
 
 // -----------------------------------------------------------SUBCLASSES AND INTERFACES------------
+
 class Point {
     x: number;
     y: number;
@@ -38,7 +39,6 @@ interface IStyle {
         opacity: number;
         display: 'inherit' | 'inline' | 'none';
         fill: string;
-        strokeWidth: number;
     };
 }
 //use this to restrict properties to needed and desired
@@ -66,9 +66,9 @@ abstract class APolygon extends Line {
     protected _y: number;
     protected _radius: number;
     protected _points: number;
-    protected _strokeWidth: number;
+    //protected _strokeWidth: number;
     protected _next: number;
-    //protected _fill: string;
+    protected _fill: string;
 
     lines: Line[];
     protected redraw: void;
@@ -93,8 +93,8 @@ export const createPolygon = (mode, el, radius=100, points=5, strokeWidth=2, nex
             this._strokeWidth = strokeWidth;
             this.redraw = this._recalc();
             this.lines = outerLines;// connection to SVG elements
-            this._x = this.x
-            this._y = this.y;
+            this._x = el.x
+            this._y = el.y;
             this._next = 1;
             this.style = el.style
         };
